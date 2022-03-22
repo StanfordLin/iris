0159415bac0cd889032937d19673d564cdc2cbe3
ichung08 <isaacchung1990@gmail.com>
2022-03-22 01:56:32 -0700

    changed apollo client code to react style

--- frontend/src/apollo-client.js
+++ frontend/src/apollo-client.js
@@ -8,28 +8,34 @@ import {
   import { LENS_API } from './config';
   import { getAuthenticationToken } from './state';
   
-  const httpLink = new HttpLink({
-    uri: LENS_API,
-    fetch,
-  });
-  
-  // example how you can pass in the x-access-token into requests using `ApolloLink`
-  const authLink = new ApolloLink((operation, forward) => {
-    const token = getAuthenticationToken();
-    console.log('jwt token:', token);
-  
-    // Use the setContext method to set the HTTP headers.
-    operation.setContext({
-      headers: {
-        'x-access-token': token ? `Bearer ${token}` : '',
-      },
+function ApolloClient() {
+    const httpLink = new HttpLink({
+        uri: LENS_API,
+        fetch,
     });
+        
+    // example how you can pass in the x-access-token into requests using `ApolloLink`
+    const authLink = new ApolloLink((operation, forward) => {
+        const token = getAuthenticationToken();
+        console.log('jwt token:', token);
+            
+        // Use the setContext method to set the HTTP headers.
+        operation.setContext({
+            headers: {
+            'x-access-token': token ? `Bearer ${token}` : '',
+            },
+        });
+        
+        // Call the next link in the middleware chain.
+        return forward(operation);
+    });
+    
+    const apolloClient = new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+    });
+    
+    return apolloClient
+}
   
-    // Call the next link in the middleware chain.
-    return forward(operation);
-  });
-  
-  export const apolloClient = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+export default ApolloClient
